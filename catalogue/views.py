ea2cba15e7bd079b7a97f5fbdc62bcece506bf3d
Rikimk <rikimkd2@gmail.com>
2022-05-09 17:47:54 +0200

    added decorators

--- catalogue/views.py
+++ catalogue/views.py
@@ -10,6 +10,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .decorators import unauthenticated_user
 
 # Create your views here.
 
@@ -40,39 +41,37 @@ class AlbumDetail(DetailView):
 def my_view(request):
     return render(request, 'catalogue/my_view.html')
 
+@unauthenticated_user
 def registerPage(request):
-    if request.user.is_authenticated:
-        return redirect('catalogue:index')
-    else:
-        form = CreateUserForm()
-        if request.method == 'POST':
-            form = CreateUserForm(request.POST)
-            if form.is_valid():
-                form.save()
-                user = form.cleaned_data.get('username')
-                messages.success(request,'Account was created for ' + user)
-                return redirect('login')
+   
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request,'Account was created for ' + user)
+            return redirect('login')
 
     context = {'form':form}
     return render(request,'registration/register.html', context)
 
+@unauthenticated_user
 def loginPage(request):
-    if request.user.is_authenticated:
-        return redirect('catalogue:index')
-    else:
-        if request.method == 'POST':
-            username = request.POST.get('username')
-            password = request.POST.get('password')
 
-            user = authenticate(request, username=username, password=password)
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
 
-            if user is not None:
-                login(request, user)
-                return redirect('catalogue:index')
-            else:
-                messages.info(request, 'Username or Password incorrect!')
-        context = {}
-        return render(request, 'registration/login.html', context)
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('catalogue:index')
+        else:
+            messages.info(request, 'Username or Password incorrect!')
+    context = {}
+    return render(request, 'registration/login.html', context)
 
 def logoutUser(request):
     logout(request)
